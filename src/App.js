aea13bee1cd3d1543f1da13f2f6e361c8c452c85
Saikat Parua <thesaikatparua@gmail.com>
2024-01-24 23:59:01 +0530

    setting up git repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './Components/basic/Header';
+import Home from './pages/home/Home'
+import MovieList from './Components/movieList/MovieList';
+import MovieDetail from './pages/movieDetail/MovieDetail'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Header />
+      <Routes>
+        <Route>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="movie/:id" element={<MovieDetail/>}></Route>
+          <Route path="movies/:type" element={<MovieList/>}></Route>
+          <Route path="/*" element={<h1>error page</h1>}></Route>
+        </Route>
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
