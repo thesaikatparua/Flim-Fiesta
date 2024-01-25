37e1a2e3e2f6fbf3cf6be7b53d1d5c7901589af5
Saikat Parua <thesaikatparua@gmail.com>
2024-01-25 14:27:23 +0530

    test

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
 
   useEffect(() => {
     fetch("https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US")
-      .then(res => res.json())
+      .then(res => res.json()) 
       .then(data => setPopularMovies(data.results))
   }, [])
 
