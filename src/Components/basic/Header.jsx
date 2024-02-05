53ed7822f8c414df3a80da40ded8610f3aeac541
Saikat Parua <thesaikatparua@gmail.com>
2024-02-05 21:31:38 +0530

    slider arrow correction

--- src/Components/basic/Header.jsx
+++ src/Components/basic/Header.jsx
@@ -1,56 +1,56 @@
-import React from "react";
-import logo from "../../asset/logo real.jpg";
-import "./Header.css";
-import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+// import React from "react";
+// import logo from "../../asset/logo real.jpg";
+// import "./Header.css";
+// import { FaSearch } from "react-icons/fa";
+// import { Link } from "react-router-dom";
 
-
-const Header = () => {
-  
-  return (
-    <>
-      <nav className="nav">
-        {/* This is logo */}
-        <Link to="/"><img src={logo} alt="logo" className="logo" /></Link>
-
-        <li>
-            <Link to="/movies/upcoming">Upcoming</Link>
-        </li>
-
-        <div className="input-wrapper">
-          <FaSearch id="search-icon" />
-          <input
-            placeholder="Search for movie..." 
-          />
-        </div>
-
-        <button>Sign In</button>
-      </nav>
-    </>
-  );
-};
-
-export default Header;
-
-// import React from 'react'
-// import './Header.css';
-// import { Link } from 'react-router-dom';
 
 // const Header = () => {
+  
 //   return (
 //     <>
-//       <div className="header">
-//         <div className="headerLeft">
-//           {/* logo start*/}
-//           <Link to="/" style={{ textDecoration: "none" }}><h1 className="header_icon">FlimFesta</h1></Link>
-//           {/* logo end*/}
-//           {/* <Link to="/movies/popular" style={{textDecoration:"none"}}><span>Popular</span></Link> */}
-//           <Link to="/movies/top_rated" style={{ textDecoration: "none" }}><span>Top Rated</span></Link>
-//           <Link to="/movies/upcoming" style={{ textDecoration: "none" }}><span>Upcoming</span></Link>
+//       <nav className="nav">
+//         {/* This is logo */}
+//         <Link to="/"><img src={logo} alt="logo" className="logo" /></Link>
+
+//         <li>
+//             <Link to="/movies/upcoming">Upcoming</Link>
+//         </li>
+
+//         <div className="input-wrapper">
+//           <FaSearch id="search-icon" />
+//           <input
+//             placeholder="Search for movie..." 
+//           />
 //         </div>
-//       </div>
+
+//         <button>Sign In</button>
+//       </nav>
 //     </>
-//   )
-// }
+//   );
+// };
 
 // export default Header;
+
+import React from 'react'
+import './Header.css';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <div className="headerLeft">
+          {/* logo start*/}
+          <Link to="/" style={{ textDecoration: "none" }}><h1 className="header_icon">FlimFesta</h1></Link>
+          {/* logo end*/}
+          {/* <Link to="/movies/popular" style={{textDecoration:"none"}}><span>Popular</span></Link> */}
+          <Link to="/movies/top_rated" style={{ textDecoration: "none" }}><span>Top Rated</span></Link>
+          <Link to="/movies/upcoming" style={{ textDecoration: "none" }}><span>Upcoming</span></Link>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,58 +1,62 @@
-import React, { useEffect, useState } from "react";
-import "./Home.css";
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+import './Home.css';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
-import { Link } from "react-router-dom";
-import MovieList from '../../Components/movieList/MovieList';
 
 const Home = () => {
-  const [popularMovies, setPopularMovies] = useState([]);
+    const [popularMovies, setPopularMovies] = useState([]);
+    const [error, setError] = useState(null);
 
-  const getData = async () => {
-    try {
-      const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US");
-      const data = await response.json();
-      setPopularMovies(data.results.slice(0, 10));
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US");
+                const data = await response.json();
+                setPopularMovies(data.results);
+            } catch (error) {
+                console.error('Error fetching data:', error.message);
+                setError('Error fetching data. Please try again later.');
+            }
+        };
 
-  useEffect(() => {
-    getData();
-  }, []);
+        fetchData();
+    }, []);
 
-  return (
-    <div className="poster">
-      <Carousel
-        showThumbs={false}
-        // autoPlay={true}
-        // transitionTime={2}
-        infiniteLoop={true}
-        showStatus={false}
-      >
-        {popularMovies.map(movie => (
-          <Link key={movie.id} style={{ textDecoration: "none", color: "white" }} className="posterImage__link" to={`/movie/${movie.id}`}>
-            <div className="posterImage">
-              <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt="movie" />
-            </div>
-            <div className="posterImage__overlay">
-              <div className="posterImage__title">{movie.original_title}</div>
-              <div className="posterImage__runtime">
-                {movie.release_date}
-                <span className="posterImage__rating">
-                  {movie.vote_average}
-                  <i className="fas fa-star" />{" "}
-                </span>
-              </div>
-              <div className="posterImage__description">{movie.overview}</div>
-            </div>
-          </Link>
-        ))}
-      </Carousel>
-      <MovieList />
-    </div>
-  );
-};
+    return (
+        <div className="poster">
+            {error ? (
+                <p>{error}</p>
+            ) : (
+                <Carousel
+                    showThumbs={false}
+                    autoPlay={true}
+                    transitionTime={3}
+                    infiniteLoop={true}
+                    showStatus={false}
+                >
+                    {popularMovies.map(movie => (
+                        <Link key={movie.id} style={{ textDecoration: "none", color: "snow" }} to={`/movie/${movie.id}`}>
+                            <div className="posterImage">
+                                <img src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path || ''}`} alt={movie?.original_title || ''} />
+                            </div>
+                            <div className="posterImage__overlay">
+                                <div className="posterImage__title">{movie?.original_title || ''}</div>
+                                <div className="posterImage__runtime">
+                                    {movie?.release_date || ''}
+                                    <span className="posterImage__rating">
+                                        {movie?.vote_average || ''}
+                                        <i className="fa-solid fa-star" />{" "}
+                                    </span>
+                                </div>
+                                <div className="posterImage__description">{movie?.overview || ''}</div>
+                            </div>
+                        </Link>
+                    ))}
+                </Carousel>
+            )}
+        </div>
+    );
+}
 
 export default Home;
