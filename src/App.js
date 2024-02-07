08c46de516b300435c0315e82cc9424e8ca1ac99
bodhisattwa bhattacharyya <bodhisattwabhattacharyya445@gmail.com>
2024-02-07 23:26:10 +0530

    footer create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Header from './Components/basic/Header';
+import Footer from './Components/footer/Footer';
 import Home from './pages/home/Home'
 import MovieList from './Components/movieList/MovieList';
 import MovieDetail from './pages/movieDetail/MovieDetail'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 const App = () => {
   return (
     <Router>
@@ -21,7 +21,7 @@ const App = () => {
           
         </Route>
       </Routes>
-  
+  <Footer />
     </Router>
   )
 }
--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,77 +1,48 @@
 import React from "react";
-import './footer.css';
+import "./Footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+function Footer() {
+  return (
+    <div className="main-footer">
+      <div className="container">
+        <div className="row">
+          {/* Column1 */}
+          <div className="col">
+            <h1>Movies Now Showing</h1>
+            <h5>
+              The Marvles | Wonka | Migration | Aquaman and the lost kingdom
+            </h5>
+            <h1>Movies Now Showing In Kolkata</h1>
+            <h5>Fighter | Dunki | Animal | Pradhan</h5>
+            <h1 className="list-unstyled">
+              <h4>Help</h4>
+            </h1>
+          </div>
 
+          {/* Column3 */}
+          <div className="footer-social">
+            <ui className="footer-social--icons">
+              <div>
+                <FaFacebookF className="icons" />
+              </div>
 
+              <div>
+                <FaYoutube className="icons" />
+              </div>
 
-const Footer=()=>{
-    return (
-        <>
-        <div className="footer">
-            <div className="sb_footer section_padding">
-                <div className="sb_footer-links">
-                    <div className="sb_footer-links-div">
-                        <h4>For business</h4>
-                        <a href="/employer">
-                            <p>Employer</p>
-                        </a>
-                        <a href="/health plan">
-                            <p>Health Plan</p>
-                        </a>
-                        <a href="/individual">
-                            <p>Individual</p>
-                        </a>
-                    </div>
-                    <div className="sb_footer-links-div">
-                        <h4>Resources</h4>
-                        <a href="/resources">
-                            <p>Resource center</p>
-                        </a>
-                        <a href="/resource">
-                            <p>Testimonials</p>
-                        </a>
-                        <a href="/resource">
-                            <p>STV</p>
-                        </a>
-                </div>
-                <div className="sb_footer-links-div">
-                        <h4>Partners</h4>
-                        <a href="/about">
-                            <p>About</p>
-                        </a>
-                        <a href="/press">
-                            <p>Press</p>
-                        </a>
-                        <a href="/career">
-                            <p>Career</p>
-                        </a>
-                        <a href="/contact">
-                            <p>Contact</p>
-                        </a>
-            </div>
-            <div className="sb_footer-links_div">
-                <h4>comming soon on</h4>
-                <div className="socialmedia">
-                    <p><image src={fb} alt=""/></p>
-                    <p><image src={youtube} alt=""/></p>
-                    <p><image src={instagram} alt=""/></p>
-                </div>
-            </div>
-
-
-        <div className="sb_footer-below">
-            <div className="sb_footer-copyright">
-                <p>
-                    @{new Date().gettingFullYear()} CodeInn. All right reserved.
-                </p>
-            </div>
-            <div className="sb_footer-below-links">
-                <a href="/terms"><p>Terms & Conditions</p></a>
-                <a href="/privacy"><p>Privacy</p></a>
-                <a href="/security"><p>Security</p></a>
-                <a href="/cookie"><p>Cookie Declaration</p></a>
-            </div>
+              <div>
+                <FaInstagram className="icons" />
+              </div>
+            </ui>
+          </div>
         </div>
-        </>
-    )
+      </div>
+      <hr />
+      <div className="row"></div>
+    </div>
+  );
 }
 
+export default Footer;
