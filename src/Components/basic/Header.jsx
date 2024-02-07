0fba6417fd0db96a7df56ba08d39ac66162a2b51
Saikat Parua <thesaikatparua@gmail.com>
2024-02-08 00:23:51 +0530

    bung solve

--- src/Components/basic/Header.jsx
+++ src/Components/basic/Header.jsx
@@ -94,26 +94,4 @@ export default Header;
 
 
 
-import React from 'react'
-import './Header.css';
-import { Link } from 'react-router-dom';
-
-const Header = () => {
-  return (
-    <>
-      <div className="header">
-        <div className="headerLeft">
-          {/* logo start*/}
-          <Link to="/" style={{ textDecoration: "none" }}><h1 className="header_icon">FlimFesta</h1></Link>
-          {/* logo end*/}
-          {/* <Link to="/movies/popular" style={{textDecoration:"none"}}><span>Popular</span></Link> */}
-          <Link to="/movies/top_rated" style={{ textDecoration: "none" }}><span>Top Rated</span></Link>
-          <Link to="/movies/upcoming" style={{ textDecoration: "none" }}><span>Upcoming</span></Link>
-        </div>
-      </div>
-    </>
-  )
-}
-
-export default Header;
 
