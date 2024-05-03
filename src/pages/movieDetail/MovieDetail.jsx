a2a0a6583d82f0c34ea28c3c2e7be1552c41962a
Saikat Parua <thesaikatparua@gmail.com>
2024-05-03 18:51:51 +0530

    Movie details modification

--- src/pages/movieDetail/MovieDetail.jsx
+++ src/pages/movieDetail/MovieDetail.jsx
@@ -29,104 +29,6 @@ const Movie = () => {
 
   return (
     <div className="movie">
-    <div className="movie-desktop">
-    <div className="movie__intro">
-        <img
-          className="movie__backdrop"
-          src={`https://image.tmdb.org/t/p/original${
-            currentMovieDetail ? currentMovieDetail.backdrop_path : ""
-          }`}
-          alt="movie poster"
-        />
-      </div>
-      <div className="movie__detail">
-        <div className="movie__detailLeft">
-          <div className="movie__posterBox">
-            <img
-              className="movie__poster"
-              src={`https://image.tmdb.org/t/p/original${
-                currentMovieDetail ? currentMovieDetail.poster_path : ""
-              }`}
-              alt="movie poster"
-            />
-          </div>
-        </div>
-        <div className="movie__detailRight">
-          <div className="movie__detailRightTop">
-            <div className="movie__name">
-              {currentMovieDetail ? currentMovieDetail.original_title : ""}
-            </div>
-            <div className="movie__tagline">
-              {currentMovieDetail ? currentMovieDetail.tagline : ""}
-            </div>
-            <div className="movie__rating">
-              {currentMovieDetail ? currentMovieDetail.vote_average : ""}{" "}
-              <i className="fas fa-star" />
-              <span className="movie__voteCount">
-                {currentMovieDetail
-                  ? "(" + currentMovieDetail.vote_count + ") votes"
-                  : ""}
-              </span>
-            </div>
-            <div className="movie__runtime">
-              {currentMovieDetail ? currentMovieDetail.runtime + " mins" : ""}
-            </div>
-            <div className="movie__releaseDate">
-              {currentMovieDetail
-                ? "Release date: " + currentMovieDetail.release_date
-                : ""}
-            </div>
-            <div className="movie__genres">
-              {currentMovieDetail &&
-                currentMovieDetail.genres &&
-                currentMovieDetail.genres.map((genre) => (
-                  <span key={genre.id} className="movie__genre">
-                    {genre.name}
-                  </span>
-                ))}
-            </div>
-  
-            <div className="movie-trailer">
-              <Moviemodal/>
-            </div>
-          </div>
-
-          <div className="movie__detailRightBottom">
-            <div className="synopsisText">Synopsis</div>
-            <div>{currentMovieDetail ? currentMovieDetail.overview : ""}</div>
-            <div className="button-container">
-              <Link
-                to={`/movie/${id}/booking`}
-                target="_blank"
-                style={{ textDecoration: "none", cursor: "pointer" }}
-              >
-                <p>Book tickets</p>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="movie__heading">Production companies</div>
-      <div className="movie__production">
-        {currentMovieDetail &&
-          currentMovieDetail.production_companies &&
-          currentMovieDetail.production_companies.map((company) =>
-            company.logo_path ? (
-              <span key={company.id} className="productionCompanyImage">
-                <img
-                  className="movie__productionComapany"
-                  src={`https://image.tmdb.org/t/p/original${company.logo_path}`}
-                  alt="production name:"
-                />
-                <span>{company.name}</span>
-              </span>
-            ) : null
-          )}
-      </div>
-    </div>
-
-
-      {/* <div className="movie-mobile">
       <div className="movie__intro">
         <img
           className="movie__backdrop"
@@ -153,6 +55,9 @@ const Movie = () => {
             <div className="movie__name">
               {currentMovieDetail ? currentMovieDetail.original_title : ""}
             </div>
+            <div className="movie__name1">
+              <p>{currentMovieDetail ? currentMovieDetail.original_title : ""}</p>
+            </div>
             <div className="movie__tagline">
               {currentMovieDetail ? currentMovieDetail.tagline : ""}
             </div>
@@ -190,7 +95,7 @@ const Movie = () => {
 
           <div className="movie__detailRightBottom">
             <div className="synopsisText">Synopsis</div>
-            <div>{currentMovieDetail ? currentMovieDetail.overview : ""}</div>
+            <div className="overview">{currentMovieDetail ? currentMovieDetail.overview : ""}</div>
             <div className="button-container">
               <Link
                 to={`/movie/${id}/booking`}
@@ -220,7 +125,6 @@ const Movie = () => {
             ) : null
           )}
       </div>
-      </div> */}
     </div>
   );
 };
